bd94371ca6e0a29a35125f97e1a2924595134ab4
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-19 23:35:51 +0530

    Welcome to stacks and queue!

---
+++ StacksQueue/StacksQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stacks and queue!");
+        }
+    }
+}
