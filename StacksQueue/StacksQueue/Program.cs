02be51eb64c7817019753a93ff3cdb9da032f2f5
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-21 16:39:16 +0530

    Using Enqueque

--- StacksQueue/StacksQueue/Program.cs
+++ StacksQueue/StacksQueue/Program.cs
@@ -6,7 +6,44 @@ namespace StacksQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to stacks and queue!");
+            Console.WriteLine("-------------Stack Operations-------------");
+
+               Console.WriteLine("***1.Push Operation***");
+                Console.WriteLine("***2.Peek And Pop Operation");
+                Console.WriteLine("3.------------Queue Operations-----------");
+                Console.WriteLine("Enter the option");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Stack stack = new Stack();
+                Queue queue = new Queue();
+                switch (num)
+                {
+                    case 1:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.Display();
+                        break;
+                    case 2:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.Pop();
+                        stack.Display();
+                        break;
+                    case 3:
+                       queue.Enqueue(56);
+                       queue.Enqueue(30);
+                       queue.Enqueue(70);
+                       queue.Display();
+                    break;
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+
+                    
+                }
+                
+            Console.ReadLine();
         }
     }
 }
